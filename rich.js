ba130d0f5600db8bcac492557a896eb9fceeb470
Piazza <richardj.piazza@gmail.com>
2017-08-22 13:00:20 -0400

    dded Code

--- rich.js
+++ rich.js
@@ -1,1 +1,30 @@
 console.log('hello from Rich at ' + Date());
+
+
+listCancers=function(id){
+    if(!id){console.warn('id not defined')
+    }else{
+        cbio.get(
+            null,
+            function(x){
+                var div = document.getElementById(id)
+                var h = ''
+                x.forEach(function(xi,i){
+                   if(i==0){
+                       h +='<h2>'+xi+'</h2>'
+                   }else{
+                       h +='<li>'+xi+'</li>'
+                   }
+
+                   //debugger 
+                })
+                div.innerHTML=h
+                
+            }
+        )    
+    }
+     
+    
+}
+
+listCancers('cancerTypesDiv')
